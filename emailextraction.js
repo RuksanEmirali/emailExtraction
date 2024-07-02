0cf4537d3de12c8d0fd0ddaa1a025e441fc826d8
Natasha Buckham <natasha.buckham@softwire.com>
2024-07-02 14:59:03 +0100

    Uses regex

--- emailextraction.js
+++ emailextraction.js
@@ -1,16 +1,11 @@
 var fs = require('fs');
 
-var counter = 0;
+// const pattern = new RegExp("/\S@softwire.com/g"); 
+const pattern = /\s[\w|.'_%+\-]+(@softwire.com)\s/g;
 
 fs.readFile('test.txt', 'utf8', function (err, data) {
-    const content = data;
-
-    for (let i = 0; i < content.length; i++) {
-        if (content.substring(i, (i + 13)) == '@softwire.com') {
-            counter += 1;
-        }
-    }
-    console.log(counter);
+    let matchedEmails = data.match(pattern);
+    console.log(matchedEmails.length);
 });
 
 

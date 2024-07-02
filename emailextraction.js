66149701a24b3c24eb64a2d57b5d8664fdb21c2d
Natasha Buckham <natasha.buckham@softwire.com>
2024-07-02 14:20:21 +0100

    Naive approach

--- emailextraction.js
+++ emailextraction.js
@@ -1,18 +1,17 @@
-//  var counter = 0
-//  
-// for(var i = 0; i < input.length; i++) {
-//   if ((input.substring(i, 13) == '@softwire.com')) { counter = counter + 1 }
-// }
-//  
-// print counter
-
 var fs = require('fs');
-var counter = 0
-let txtcont = fs.readFile('test.txt','utf8', function (err, data) {
-    // Display the file content
-    return data;
-})
- console.log(txtcont)
+
+var counter = 0;
+
+fs.readFile('test.txt', 'utf8', function (err, data) {
+    const content = data;
+
+    for (let i = 0; i < content.length; i++) {
+        if (content.substring(i, (i + 13)) == '@softwire.com') {
+            counter += 1;
+        }
+    }
+    console.log(counter);
+});
 
 
 
